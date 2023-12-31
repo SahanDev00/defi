24cbf79a69ad183e85dbd6ecb7827109c6fae0f6
SahanDev00 <dumidus86@gmail.com>
2023-12-31 17:59:25 +0530

    Added Footer Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Signup from "./components/Signup";
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <Featured/>
     <Signup/>
+    <Footer/>
     </>
   );
 }
